aefd4655b069bf49fb04147ac519e2c37a448efe
Richiedav23 <richardanthonydavenport@gmail.com>
2026-06-25 14:50:47 +0100

    Rename knock-logo.png to knocked-site/public

--- knocked-site/src/App.tsx
+++ knocked-site/src/App.tsx
@@ -693,7 +693,7 @@ display: grid;
   }}
 >
   <img 
-    src="/Kncokdbanner2.png"
+    src="/Knockdbanner3.png"
     alt="Knockd"
     style={{ 
       width: "100%", 
@@ -1070,7 +1070,7 @@ display: grid;
             margin: "50px 0 20px" 
           }}>
             <img 
-              src="/Kncokdbanner2.png" 
+              src="/Knockdbanner3.png" 
               alt="Knockd"
               style={{ width: "340px", opacity: 0.95 }}
             />
